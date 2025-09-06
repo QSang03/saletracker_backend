6a6f5726e048c125a4a989712245a80e71bfec7e
ngocthach <ngocthach.2004bp@gmail.com>
2025-09-06 12:06:50 +0700

    feat(debt-entity): add missing indexes for improved query performance on updated_at and employee_code_raw

--- src/debts/debt.entity.ts
+++ src/debts/debt.entity.ts
@@ -25,6 +25,8 @@ export enum DebtStatus {
 @Index('idx_employee_code_raw', ['employee_code_raw'])
 @Index('idx_debt_config_id', ['debt_config'])
 @Index('idx_due_date', ['due_date'])
+@Index('idx_debts_updated_status', ['updated_at', 'status', 'deleted_at'])
+@Index('idx_debts_employee_status', ['employee_code_raw', 'status', 'deleted_at'])
 export class Debt {
   @PrimaryGeneratedColumn()
   id: number;
